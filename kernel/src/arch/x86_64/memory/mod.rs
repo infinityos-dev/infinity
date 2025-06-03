722a6ead69a1361d634133fe08ab3e0d4150acfd
Viktor Popp <viktorpopp.dev@gmail.com>
2025-06-03 14:07:13 +0200

    Made the allocator size a constant

--- kernel/src/arch/x86_64/memory/mod.rs
+++ kernel/src/arch/x86_64/memory/mod.rs
@@ -25,20 +25,18 @@ use talc::{ErrOnOom, Talc, Talck};
 
 pub mod hhdm;
 
+const GLOBAL_ALLOCATOR_SIZE: u64 = 4 * 0x400 * 0x400; // 4 MiB
+
 // This tells Rust that global allocations will use this static variable's allocation functions
 #[global_allocator]
 static GLOBAL_ALLOCATOR: Talck<spin::Mutex<()>, ErrOnOom> = Talck::new(Talc::new(ErrOnOom));
 
 pub fn init(memory_map: &'static MemoryMapResponse, hhdm_offset: hhdm::HhdmOffset) {
-    let global_allocator_size = {
-        // 4 MiB
-        4 * 0x400 * 0x400
-    };
     let global_allocator_physical_start = memory_map
         .entries()
         .iter()
         .find(|entry| {
-            entry.entry_type == EntryType::USABLE && entry.length >= global_allocator_size
+            entry.entry_type == EntryType::USABLE && entry.length >= GLOBAL_ALLOCATOR_SIZE
         })
         .unwrap()
         .base;
@@ -46,7 +44,7 @@ pub fn init(memory_map: &'static MemoryMapResponse, hhdm_offset: hhdm::HhdmOffse
     let global_allocator_mem = unsafe {
         slice::from_raw_parts_mut::<MaybeUninit<u8>>(
             (u64::from(hhdm_offset) + global_allocator_physical_start) as *mut _,
-            global_allocator_size as usize,
+            GLOBAL_ALLOCATOR_SIZE as usize,
         )
     };
 
