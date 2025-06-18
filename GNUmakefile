e2e395eee8f346221dc5e010cc0f260d0d971f30
Viktor Popp <viktorpopp.dev@gmail.com>
2025-06-18 21:41:46 +0200

    APIC mapping does not work

--- GNUmakefile
+++ GNUmakefile
@@ -4,7 +4,7 @@ MAKEFLAGS += -rR
 override USER_VARIABLE = $(if $(filter $(origin $(1)),default undefined),$(eval override $(1) := $(2)))
 
 $(call USER_VARIABLE,KARCH,x86_64)
-$(call USER_VARIABLE,QEMUFLAGS,-m 2G -d int -D log/interrupts.txt -M smm=off -no-reboot -no-shutdown -serial stdio -smp 2)
+$(call USER_VARIABLE,QEMUFLAGS,-m 2G -d int -D log/interrupts.txt -M smm=off -serial stdio -smp 2)
 
 override IMAGE_NAME := hexium_os-$(KARCH)
 
--- kernel/src/arch/x86_64/acpi/mod.rs
+++ kernel/src/arch/x86_64/acpi/mod.rs
@@ -3,17 +3,26 @@ use alloc::boxed::Box;
 use handler::KernelAcpiHandler;
 use limine::response::RsdpResponse;
 
-use crate::{arch::limine::RSDP_REQUEST, trace};
+use crate::{arch::limine::RSDP_REQUEST, debug, trace};
 
+pub mod apic;
 pub mod handler;
 
 pub fn init() {
     let rsdp = RSDP_REQUEST.get_response().unwrap();
-    let acpi_tables = unsafe { get_acpi_tables(rsdp) }
+    let acpi_tables = unsafe { get_acpi_tables(rsdp) };
+    let signatures = acpi_tables
         .headers()
         .map(|header| header.signature)
         .collect::<Box<[_]>>();
-    trace!("ACPI Tables: {acpi_tables:?}");
+    trace!("ACPI Tables: {signatures:?}");
+
+    debug!(
+        "ACPI platform information\n{:#?}",
+        acpi_tables.platform_info().unwrap()
+    );
+
+    apic::init(&acpi_tables);
 }
 
 /// # Safety
